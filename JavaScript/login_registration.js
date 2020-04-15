76b3bdb47e2f67e675b5e2c65b097371f8b9b0b5
roman grig <49118286+romangrig17@users.noreply.github.com>
2020-04-15 18:14:44 +0300

    Need to make jQuery_Reg

---
+++ JavaScript/login_registration.js
@@ -0,0 +1,7 @@
+
+$(function(){
+    $('#submit_reg').on('click', function (e) {
+        document.getElementById('welcome').style.display = "none";
+        document.getElementById('registration_page').style.display = "none";
+    });
+});
--- JavaScript/welcome.js
+++ JavaScript/welcome.js
@@ -1,13 +1,22 @@
-function show_welcome_page()
-{
-    document.getElementById('welcome').style.display = "block";
-}
-function welcome_login()
-{
 
-}
+/**
+ * On click open the login div
+ */
+$(function(){
+    $('#login_btn').on('click', function (e) {
+        document.getElementById('welcome').style.display = "none";
+        document.getElementById('login_page').style.display = "block";
+    });
+});
 
-function welcome_login()
-{
+/**
+ * On click open the registration div
+ */
+$(function(){
+    $('#registration_btn').on('click', function (e) {
+        document.getElementById('welcome').style.display = "none";
+        document.getElementById('registration_page').style.display = "block";
+        
+    });
+});
 
-}
