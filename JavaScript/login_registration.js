a53f958710b7db6e8e10128239d6d03d60c6784d
roman grig <49118286+romangrig17@users.noreply.github.com>
2020-04-18 01:27:33 +0300

    Registaion Page Full

--- JavaScript/login_registration.js
+++ JavaScript/login_registration.js
@@ -1,24 +1,55 @@
+/**
+ * Registration
+ */
 $.validator.setDefaults({
     submitHandler: function() {
         alert("submitted!");
     }
 });
 
+$.validator.addMethod("pwcheck", function(value) {
+    var reg1 = /[a-zA-Z]/; //check if there is a number
+    return reg1.test(value) // consists of only these
+        && /\d/.test(value) // has a digit
+ });
+
+$.validator.addMethod("ncheck", function(value) {
+    var reg2 = /[0-9]/; //check if there is a number
+    if(reg2.test(value))
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+ });
+
+
 $("#signupForm").validate({
+    errorElement: 'div',
     rules: {
-        firstname: "required",
-        lastname: "required",
-        username: {
+        firstname: {
             required: true,
-            minlength: 2
+            ncheck: true
+            
+        },
+        lastname: {
+            required: true,
+            ncheck: true
+            
+        },
+        username: {
+            required: true
         },
         password: {
             required: true,
-            minlength: 5
+            minlength: 6,
+            pwcheck: true
         },
         confirm_password: {
             required: true,
-            minlength: 5,
+            minlength: 6,
             equalTo: "#password"
         },
         email: {
@@ -27,22 +58,28 @@ $("#signupForm").validate({
         },
     },
     messages: {
-        firstname: "Please enter your firstname",
-        lastname: "Please enter your lastname",
+        firstname: {
+            required: "* Please enter a first name",
+            ncheck: "* No numbers allowed on name"
+        },
+        lastname: {
+            required: "* Please enter a last name",
+            ncheck: "* No numbers allowed on name"
+        },
         username: {
-            required: "Please enter a username",
-            minlength: "Your username must consist of at least 2 characters"
+            required: "* Please enter a username"
         },
         password: {
-            required: "Please provide a password",
-            minlength: "Your password must be at least 5 characters long"
+            required: "* Please provide a password",
+            minlength: "* Your password must be at least 5 characters long",
+            pwcheck: "* The password must conatins number and characters"
         },
         confirm_password: {
-            required: "Please provide a password",
-            minlength: "Your password must be at least 5 characters long",
-            equalTo: "Please enter the same password as above"
+            required: "* Please provide a password",
+            minlength: "* Your password must be at least 5 characters long",
+            equalTo: "* Please enter the same password as above"
         },
-        email: "Please enter a valid email address",
+        email: "* Please enter a valid email address",
     }
 });
 
@@ -53,3 +90,8 @@ $("#username").focus(function() {
         this.value = firstname + "." + lastname;
     }
 });
+
+
+/**
+ * Login
+ */
