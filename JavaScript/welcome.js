e8e715618912f578ac4891e67cfbb0df8efa943f
roman grig <49118286+romangrig17@users.noreply.github.com>
2020-04-22 03:16:31 +0300

    Header

--- JavaScript/welcome.js
+++ JavaScript/welcome.js
@@ -4,6 +4,12 @@
  */
 function loginShow(){
     $("div").hide();
+
+    $("#header").show();
+    $("#header-left").show();
+    $("#header-center").show();
+    $("#header-right").show();
+    
     $("#login_page").show();
 
 };
@@ -13,6 +19,12 @@ function loginShow(){
  */
 function registrationShow(){
     $("div").hide();
+
+    $("#header").show();
+    $("#header-left").show();
+    $("#header-center").show();
+    $("#header-right").show();
+
     $("#registration_page").show();
     
 };
